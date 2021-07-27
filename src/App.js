880c1560ff0f63d06d8072b10c50fb1b8386286e
Eriberto Guzman <eriberto.guzman@outlook.com>
2021-07-27 16:29:29 -0400

    Finish lecture ntoes

--- src/App.js
+++ src/App.js
@@ -11,11 +11,31 @@ class App extends Component {
     saved: []
   }
 
+  componentDidMount(){
+    fetch('http://localhost:3001/recipes')
+      .then(resp => resp.json())
+      .then(recipes => this.setState({recipes: recipes}))
+  }
+
+  handlePage = (e) => {
+    this.setState({page: e.target.id})
+  }
+  
+  addRecipe = (id) => {
+    const foundRecipe = this.state.recipes.find( recipe => recipe.id === id)
+    this.setState((prevState) => {
+      return{
+        //saved: prevState.saved.concat(foundRecipe)
+        saved: [...prevState.saved, foundRecipe]
+      }
+    }, () => console.log(this.state))
+  }
+
   render() {
     return (
-      <div class="app">
-        <NavBar /> 
-        <RecipesContainer /> 
+      <div className="app">
+        <NavBar handleClick={this.handlePage} /> 
+        {this.state.page === "recipes" ? <RecipesContainer addRecipe={this.addRecipe} recipes={this.state.recipes} saved={this.state.saved} /> : <SavedRecipes saved={this.state.saved} /> }
       </div>
     )
   }
