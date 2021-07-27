e6de985836f68bf929e11b6c28e1473b5209bcfc
Eriberto Guzman <eriberto.guzman@outlook.com>
2021-07-27 13:52:50 -0400

    Fixs styling for filter bar

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -3,6 +3,7 @@ import React from 'react'
 function FilterBar() {
     return (
         <div id="filter-bar">
+            <label htmlFor="filter">Search by Recipe Title: </label>
             <input type="text"/>
         </div>
     )
